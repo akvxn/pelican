95cd5c140e472c2df82e80610b762f3348089acc
Alfaa <74573419+akvxn@users.noreply.github.com>
2025-07-30 04:40:43 +0200

    ajout d'un href

---
+++ clientLogto.js
@@ -0,0 +1,8 @@
+import { LogtoClient } from '@logto/browser';
+
+export const LogtoClient = new LogtoClient({
+    endpoint: 'https://b5q39c.logto.app/',
+    appId: 'jg81pom9gvg9fgoj94uln',
+    redirectUri:'xn--plican-bva.fr/callback',
+
+});
