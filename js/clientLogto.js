1ae01892f769aaa0345870381fc0de8efd70a18f
Alfaa <74573419+akvxn@users.noreply.github.com>
2025-07-30 05:20:05 +0200

    ajout du coté client de logto

---
+++ js/clientLogto.js
@@ -0,0 +1,7 @@
+import { LogtoClient } from '@logto/browser';
+
+export const logtoClient = new LogtoClient({
+  endpoint: 'https://b5q39c.logto.app/',
+  appId: 'jg81pom9gvg9fgoj94uln',
+  redirectUri: 'https://pélican.fr/callback', // adapte ici ton domaine
+});
