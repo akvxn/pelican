42fc2206470acb50ec689c940b9d8e307f876cb8
Alfaa <74573419+akvxn@users.noreply.github.com>
2025-07-30 05:21:51 +0200

    déplacement du fichier dans /js

--- clientLogto.js
+++
@@ -1,8 +0,0 @@
-import { LogtoClient } from '@logto/browser';
-
-export const LogtoClient = new LogtoClient({
-    endpoint: 'https://b5q39c.logto.app/',
-    appId: 'jg81pom9gvg9fgoj94uln',
-    redirectUri:'xn--plican-bva.fr/callback',
-
-});
