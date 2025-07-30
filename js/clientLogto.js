19823aa91a2c21b4124d0d994409463b51652f06
Alfaa <74573419+akvxn@users.noreply.github.com>
2025-07-30 07:35:39 +0200

    ajout d'un cdn

--- js/clientLogto.js
+++ js/clientLogto.js
@@ -1,7 +1,7 @@
-import { LogtoClient } from '@logto/browser';
+import Logto from 'https://cdn.jsdelivr.net/npm/@logto/browser/+esm';
 
-export const logtoClient = new LogtoClient({
+export const logtoClient = new Logto({
   endpoint: 'https://b5q39c.logto.app/',
   appId: 'jg81pom9gvg9fgoj94uln',
-  redirectUri: 'https://pélican.fr/callback', // adapte ici ton domaine
+  redirectUri: 'https://xn--plican-bva.fr/callback' // remplace avec ton vrai domaine
 });
