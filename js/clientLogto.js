52b4bc8b4a2cbf3a0e0038fb1a7722c0b103c2fb
Alfaa <74573419+akvxn@users.noreply.github.com>
2025-08-01 01:09:43 +0200

    déplacement de clientlogto.js

--- js/clientLogto.js
+++
@@ -1,8 +0,0 @@
-import LogtoClient from 'https://cdn.jsdelivr.net/npm/@logto/browser/+esm';
-
-export const logtoClient = new LogtoClient({
-  endpoint: 'https://b5q39c.logto.app/',
-  appId: 'jg81pom9gvg9fgoj94uln',
-  redirectUri: 'https://xn--plican-bva.fr/callback',
-  scopes: ['openid', 'profile', 'offline_access'],
-});
